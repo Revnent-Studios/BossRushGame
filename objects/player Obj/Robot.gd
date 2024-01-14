f439f6278948bf5080b3af59c88685f2ce934690
Pratyush Soni <pratyushsoni2004@gmail.com>
2024-01-15 00:45:49 +0530

    Formatting of Robot Script

--- objects/player Obj/Robot.gd
+++ objects/player Obj/Robot.gd
@@ -1,39 +1,51 @@
 extends CharacterBody2D
 
+#region Constants&Variables
+const MOVEMENT_SPEED = 500.0
+const max_jump_velocity = -800.0
+const  gravity = 1960
+var min_jump_velocity = -600.0
+var jump_ability = 0
+#endregion
 
-const SPEED = 500.0
-var JUMP_VELOCITY_Max = -800.0
-var JUMP_VELOCITY_MIN = -600.0
-var jumpAbility = 0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = 1960
-
-
+#region DriverCode
 func _physics_process(delta):
-	# Add the gravity.
+	# Add gravity if not on the floor
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		jumpAbility+=1
-	if Input.is_action_pressed("jump") and JUMP_VELOCITY_MIN>-800 and jumpAbility<1:
-		JUMP_VELOCITY_MIN-=6000*delta
-		velocity.y = max(JUMP_VELOCITY_Max,JUMP_VELOCITY_MIN)
-	if is_on_floor():
-		jumpAbility = 0
-		
-	if Input.is_action_just_released("jump"):
-		JUMP_VELOCITY_MIN =0.0		
+	# Handle jumping
+	jump(delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Handle horizontal movement
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * MOVEMENT_SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, MOVEMENT_SPEED)
 
+	# Move and slide to handle collisions
 	move_and_slide()
+#endregion
+
+func jump(delta):
+	# Increase jump ability when jump is pressed
+	if Input.is_action_just_pressed("jump"):
+		jump_ability += 1
+
+	# Adjust jump velocity when jump is held
+	if Input.is_action_pressed("jump") and min_jump_velocity > -800 and jump_ability < 1:
+		min_jump_velocity -= 6000 * delta
+		velocity.y = max(max_jump_velocity, min_jump_velocity)
+
+	# Reset jump ability when on the floor
+	if is_on_floor():
+		jump_ability = 0
+
+	# Reset jump velocity when jump is released
+	if Input.is_action_just_released("jump"):
+		min_jump_velocity = 0.0
+
+#TODO
+#Add Dash Mechanic
