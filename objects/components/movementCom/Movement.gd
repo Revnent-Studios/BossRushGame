9d250c101dcb44e26532c3e3d9e8e5d00bb5c524
Pratyush Soni <pratyushsoni2004@gmail.com>
2024-01-15 19:37:23 +0530

    added player movement class

---
+++ objects/components/movementCom/Movement.gd
@@ -0,0 +1,46 @@
+class_name Movement
+extends CharacterBody2D
+
+# Exported Variables
+const MOVEMENT_SPEED : float = 500.0
+const max_jump_velocity : float = -800.0
+const gravity : float = 1960.0
+var min_jump_velocity : float = -600.0
+
+# Internal Variables
+var jump_ability : int = 0
+
+func get_gravity(delta: float,player:CharacterBody2D) -> void:
+	# Add gravity if not on the floor
+	if not player.is_on_floor():
+		player.velocity.y += gravity * delta
+
+func get_in(delta: float,player:CharacterBody2D) -> void:
+	# Handle horizontal movement
+	var direction : float = Input.get_axis("left", "right")
+
+	if direction:
+		player.velocity.x = direction * MOVEMENT_SPEED
+	else:
+		player.velocity.x = move_toward(player.velocity.x, 0, MOVEMENT_SPEED)
+	
+
+func jump(delta: float,player:CharacterBody2D) -> void:
+	# Increase jump ability when jump is pressed
+	if Input.is_action_just_pressed("jump"):
+		jump_ability += 1
+
+	# Adjust jump velocity when jump is held
+	if Input.is_action_pressed("jump") and min_jump_velocity > -800 and jump_ability < 1:
+		min_jump_velocity -= 6000 * delta
+		player.velocity.y = max(max_jump_velocity, min_jump_velocity)
+	
+
+	# Reset jump ability when on the floor
+	if player.is_on_floor():
+		jump_ability = 0
+
+	# Reset jump velocity when jump is released
+	if Input.is_action_just_released("jump"):
+		min_jump_velocity = 0.0
+
--- objects/player Obj/birju.gd
+++ objects/player Obj/birju.gd
@@ -1,51 +1,20 @@
 extends CharacterBody2D
 
-#region Constants&Variables
-const MOVEMENT_SPEED = 500.0
-const max_jump_velocity = -800.0
-const  gravity = 1960
-var min_jump_velocity = -600.0
-var jump_ability = 0
-#endregion
+# Create an instance of the Movement class
+var player_Motion : Movement
 
+func _ready():
+	# Instantiate the Movement class
+	player_Motion = Movement.new()
+	
+	# Add the Movement instance as a child of the current node
+	add_child(player_Motion)
 
-#region DriverCode
 func _physics_process(delta):
-	# Add gravity if not on the floor
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	# Call the methods on the player_Motion instance
+	player_Motion.get_gravity(delta,$".")
+	player_Motion.get_in(delta,$".")
+	player_Motion.jump(delta,$".")
 
-	# Handle jumping
-	jump(delta)
-
-	# Handle horizontal movement
-	var direction = Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * MOVEMENT_SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, MOVEMENT_SPEED)
-
-	# Move and slide to handle collisions
+	# Continue with your other physics process logic
 	move_and_slide()
-#endregion
-
-func jump(delta):
-	# Increase jump ability when jump is pressed
-	if Input.is_action_just_pressed("jump"):
-		jump_ability += 1
-
-	# Adjust jump velocity when jump is held
-	if Input.is_action_pressed("jump") and min_jump_velocity > -800 and jump_ability < 1:
-		min_jump_velocity -= 6000 * delta
-		velocity.y = max(max_jump_velocity, min_jump_velocity)
-
-	# Reset jump ability when on the floor
-	if is_on_floor():
-		jump_ability = 0
-
-	# Reset jump velocity when jump is released
-	if Input.is_action_just_released("jump"):
-		min_jump_velocity = 0.0
-
-#TODO
-#Add Dash Mechanic
