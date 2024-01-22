42a5b91090aa494afacdc616f887cc223abc8c7c
= <shashwatthegamer332@gmail.com>
2024-01-22 22:39:13 +0530

    added attack hitboxes

--- objects/components/Boss/damage.gd
+++ objects/components/Boss/damage.gd
@@ -1,11 +1,19 @@
 class_name boss
 
+var health = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+# setter
+func setHealth(hp):
+	health = hp
 
+# getter
+func getHealth():
+	return health
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
