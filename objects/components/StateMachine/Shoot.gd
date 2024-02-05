4fc674fb9709c927aa266b8bb1c890b19992cdbd
= <shashwatthegamer332@gmail.com>
2024-02-05 10:45:32 +0530

    working warden animations

--- objects/components/StateMachine/Shoot.gd
+++ objects/components/StateMachine/Shoot.gd
@@ -4,6 +4,9 @@ class_name Shoot
 @onready var birju = owner.get_parent().find_child("Robot")
 @onready var warden = owner
 @onready var animation_tree = $"../../AnimationTree"
+@onready var wardensprite = $"../../Warden"
+@onready var warden_shoot = $"../../WardenShoot"
+
 var distance
 func enter():
 	distance = birju.position.x - warden.position.x
@@ -11,12 +14,21 @@ func enter():
 		facing.x = 1
 	elif(distance<0):
 		facing.x = -1
-	print("Entered attack state")
+
+	print("Entered Shoot state")
 	animation_tree["parameters/conditions/shooting"] = true
 	animation_tree["parameters/Shoot/blend_position"] = facing
-	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_timer(0.7).timeout
+	wardensprite.visible = false
+	warden_shoot.visible = true
+	animation_tree["parameters/conditions/shooting"] = false
+	animation_tree["parameters/conditions/fire"] = true
+	animation_tree["parameters/ShootEnd/blend_position"] = facing
+	await get_tree().create_timer(0.7).timeout
 	transition.emit(self, "Follow")
 
 func exit():
-	animation_tree["parameters/conditions/shooting"] = false
+	wardensprite.visible = true
+	warden_shoot.visible = false
+	animation_tree["parameters/conditions/fire"] = false
 	warden.flag = true
